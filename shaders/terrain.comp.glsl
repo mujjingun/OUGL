4d6f9fc0e7781896584660c1ee7407680526faea
Gun Park <mujjingun@gmail.com>
2019-03-22 22:02:08 +0900

    add bicubic filtering

--- shaders/terrain.comp.glsl
+++ shaders/terrain.comp.glsl
@@ -213,6 +213,44 @@ bool skip(Update update, Lod lod, vec2 modUv) {
     return false;
 }
 
+vec4 cubic(float v)
+{
+    vec4 n = vec4(1.0, 2.0, 3.0, 4.0) - v;
+    vec4 s = n * n * n;
+    float x = s.x;
+    float y = s.y - 4.0 * s.x;
+    float z = s.z - 4.0 * s.y + 6.0 * s.x;
+    float w = 6.0 - x - y - z;
+    return vec4(x, y, z, w);
+}
+
+vec4 filt(vec2 texcoord, vec2 texscale, int idx)
+{
+    float fx = fract(texcoord.x);
+    float fy = fract(texcoord.y);
+    texcoord.x -= fx;
+    texcoord.y -= fy;
+
+    vec4 xcubic = cubic(fx);
+    vec4 ycubic = cubic(fy);
+
+    vec4 c = vec4(texcoord.x - 0.5, texcoord.x + 1.5, texcoord.y - 0.5, texcoord.y + 1.5);
+    vec4 s = vec4(xcubic.x + xcubic.y, xcubic.z + xcubic.w, ycubic.x + ycubic.y, ycubic.z + ycubic.w);
+    vec4 offset = c + vec4(xcubic.y, xcubic.w, ycubic.y, ycubic.w) / s;
+
+    vec4 sample0 = texture(tex, vec3(vec2(offset.x, offset.z) * texscale, idx));
+    vec4 sample1 = texture(tex, vec3(vec2(offset.y, offset.z) * texscale, idx));
+    vec4 sample2 = texture(tex, vec3(vec2(offset.x, offset.w) * texscale, idx));
+    vec4 sample3 = texture(tex, vec3(vec2(offset.y, offset.w) * texscale, idx));
+
+    float sx = s.x / (s.x + s.y);
+    float sy = s.z / (s.z + s.w);
+
+    return mix(
+        mix(sample3, sample2, sx),
+        mix(sample1, sample0, sx), sy);
+}
+
 void main() {
     ivec3 pixel_coords = ivec3(gl_GlobalInvocationID.xyz);
     vec2 imgSize = vec2(imageSize(image).xy);
@@ -245,12 +283,12 @@ void main() {
         float height = ridgeWithOctaves(pos * 2.0, 20);
         vec4 pixel = vec4(height, 0.0, 0.0, 1.0);
 
-        // upsample parent
+        // bicubic filter upsample parent
         Lod plod = uLods[lod.parentIdx];
         vec2 pOffset = ((lod.center - plod.center) / lod.scale + 1) / 4;
         vec2 pUv = modUv / 2 + pOffset + plod.origin;
-        vec2 pmodUv = fract(pUv);
-        //pixel = texture(tex, vec3(pmodUv, plod.imgIdx));
+        vec2 pmodUv = fract(pUv) * imgSize;
+        pixel = filt(pmodUv, 1 / imgSize, plod.imgIdx);
 
         // output to a specific pixel in the image
         pixel_coords.z = lod.imgIdx;
